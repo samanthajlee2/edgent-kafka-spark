3ebe26233769513e3a0cb5544eb29fb3bf463757
samanthajlee2 <slee.bhs@gmail.com>
2018-03-01 00:04:05 -0500

    compiles but script not working

---
+++ app-runaskafkaclient.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+DIR=..
+
+UBER_JAR=`echo ${DIR}/target/my-app-*-uber.jar`
+
+# Runs the Kafka Publisher or Subscriber Client
+#
+# ./runkafkaclient.sh pub
+# ./runkafkaclient.sh sub
+# ./runkafkaclient.sh -h
+
+export CLASSPATH=${UBER_JAR}
+
+java com.mycompany.app.KafkaClient $@
