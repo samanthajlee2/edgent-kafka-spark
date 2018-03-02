310124b4e4efb73d321b6ddd3996c26a2996132d
Steven Haussmann <hausss@rpi.edu>
2018-03-01 21:14:08 -0500

    tidied scripts up

--- run-tempSensorApp.sh
+++ run-tempSensorApp.sh
@@ -1,42 +1,8 @@
-#!/bin/bash
-#
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+UBER_JAR=target/*-uber.jar
 
-APP_DIR=.
+echo $UBER_JAR
 
-UBER_JAR=`echo ${APP_DIR}/target/*-uber.jar`
+SAMPLE_PACKAGE_BASE=com.mycompany.app
+CLASS_NAME=${SAMPLE_PACKAGE_BASE}.TempSensorApp
 
-FQ_MAIN_CLASS=com.mycompany.app.TempSensorApp
-
-USAGE="usage: [--main <main-class>] [<args...>]"
-
-if [ "$1" = "--main" ]; then
-  shift;
-  if [ $# = 0 ]; then
-    echo ${USAGE}
-    exit 1;
-  fi 
-  FQ_MAIN_CLASS=$1; shift
-fi
-
-# Runs the application
-#
-# ./app-run.sh
-
-export CLASSPATH=${UBER_JAR}
-
-java ${FQ_MAIN_CLASS} "$*"
+java -cp ${UBER_JAR} "${CLASS_NAME}" "$@"
